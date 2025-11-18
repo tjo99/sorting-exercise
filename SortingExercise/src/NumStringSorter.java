4be896cf9906e2513bd5680acb3babf677344dea
tjo99 <47487879+tjo99@users.noreply.github.com>
2025-11-17 17:01:37 -0800

    Add files via upload

--- SortingExercise/src/NumStringSorter.java
+++ SortingExercise/src/NumStringSorter.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class NumStringSorter {
 	public static void main(String[] args) {
+		//number sort
 		ArrayList<Integer> nums = new ArrayList<Integer>();
 		Random rn = new Random();
 		
+		//randomly generate three numbers to sort
 		int n1 = rn.nextInt(100);
 		System.out.println("First number: " + n1);
 		nums.add(n1);
@@ -17,14 +19,18 @@ public class NumStringSorter {
 		System.out.println("Third number: " + n3);
 		nums.add(n3);
 		
+		//sort the numbers
 		Collections.sort(nums);
 		
+		//print result
 		System.out.println("Sorted number list: " + nums);
 		System.out.println();
 		
+		//character sort
 		ArrayList<Character> chars = new ArrayList<Character>();
 		Random rc = new Random();
-		
+
+		//randomly generate three letters to sort
 		char c1 = (char) (rc.nextInt(26) + 'a');
 		System.out.println("First letter: " + c1);
 		chars.add(c1);
@@ -36,9 +42,11 @@ public class NumStringSorter {
 		char c3 = (char) (rc.nextInt(26) + 'a');
 		System.out.println("Third letter: " + c3);
 		chars.add(c3);
-		
+
+		//sort the letters
 		Collections.sort(chars);
-		
+
+		//print result
 		System.out.println("Sorted letter list: " + chars);
 	}
 }
